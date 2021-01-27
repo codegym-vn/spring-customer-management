eb03cba5145ead011f0b85394c29070103c48288
quan200699 <51079189+quan200699@users.noreply.github.com>
2021-01-27 16:21:20 +0700

    add paging and sorting

--- src/main/java/com/codegym/cms/model/Customer.java
+++ src/main/java/com/codegym/cms/model/Customer.java
@@ -5,8 +5,9 @@ import javax.persistence.*;
 @Entity
 @Table(name = "customers")
 public class Customer {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
@@ -15,8 +16,7 @@ public class Customer {
     private Province province;
 
 
-    public Customer() {
-    }
+    public Customer() {}
 
     public Customer(String firstName, String lastName) {
         this.firstName = firstName;
